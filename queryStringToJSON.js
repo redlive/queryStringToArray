87e9e9fd914655058551f8261522fe5dd1793b5b
redlive <vilder.eugene@gmail.com>
2013-12-02 19:46:21 +0200

    Update queryStringToJSON.js

--- queryStringToJSON.js
+++ queryStringToJSON.js
@@ -1,29 +1,26 @@
 /*
- * Javascript convertation of window query string to JSON
- * https://github.com/redlive/queryStringToJSON
- *
- * Created; 02/12/2013 by Vilder Eugene
- */
- 
+* Javascript convertation of window query string to JSON
+* https://github.com/redlive/queryStringToJSON
+*
+* Created; 02/12/2013 by Vilder Eugene
+*/
+
 (function(global){
-  "use strict";  
+	"use strict";  
 
-  global.location.queryStringToJSON = function(){
-    var queryString = window.location.search,
-        parseQueryString = function(q) {
-          var queryStringArr = q.slice(1).split('&');
+	global.location.queryStringToJSON = function(){
+		var queryString = global.location.pathname.replace('?','&').replace('&&','&');
+		var parseQueryString = function(q) {
+			var queryStringArr = q.split('&').slice(1);
+			return queryStringArr.map(function(val,ind){
+				var q = val.split('='),        
+					res = {};
+				res.key = q[0];
+				res.value = q[1];
+				return res;        
+			});
+		}
+	return queryString.length == 0 ? [] : parseQueryString(queryString);
+}
 
-          return queryStringArr.map(function(val,ind){
-            var q = val.split('='),	
-                res = {};
-            res.key = q[0];
-            res.value = q[1];
-          
-            return res;	
-          });
-        }
-
-    return queryString.length == 0 ? [] : parseQueryString(queryString);
-  }
-  
 })(window);
