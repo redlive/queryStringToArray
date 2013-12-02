ded61982ebeaf6336018b2a65a5224474ac23861
redlive <vilder.eugene@gmail.com>
2013-12-02 19:19:03 +0200

    Create queryStringToJSON.js

---
+++ queryStringToJSON.js
@@ -0,0 +1,29 @@
+/*
+ * Javascript convertation of window query string to JSON
+ * https://github.com/redlive/queryStringToJSON
+ *
+ * Created; 02/12/2013 by Vilder Eugene
+ */
+ 
+(function(global){
+  "use strict";  
+
+  global.location.queryStringToJSON = function(){
+    var queryString = window.location.search,
+        parseQueryString = function(q) {
+          var queryStringArr = q.slice(1).split('&');
+
+          return queryStringArr.map(function(val,ind){
+            var q = val.split('='),	
+                res = {};
+            res.key = q[0];
+            res.value = q[1];
+          
+            return res;	
+          });
+        }
+
+    return queryString.length == 0 ? [] : parseQueryString(queryString);
+  }
+  
+})(window);
